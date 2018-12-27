a024f31dd5307776841ae77bd5ec140cf6f104c7
Or <orsaa@post.bgu.ac.il>
2018-12-27 22:24:16 +0200

    flowchart printed

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 import $ from 'jquery';
-import {parseCode} from './code-analyzer';
+import {parseCode,doyourstuff} from './code-analyzer';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
+        doyourstuff(parsedCode);
     });
 });
